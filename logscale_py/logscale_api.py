c866e97bf97c0eabdd19f49194923a0f2c50ab66
milesjniemeyer <miles@mjniemeyer.com>
2024-06-25 14:40:20 -0400

    query_job endpoint

--- logscale_py/logscale_api.py
+++ logscale_py/logscale_api.py
@@ -20,3 +20,30 @@ class LogScaleAPI:
                 'timeZoneOffsetMinutes': time_zone_offset_minutes}
         result = self._rest_adapter.post(endpoint=f'/repositories/{repo}/query', data=data)
         return QueryList([Query.from_dict(item) for item in result.data])
+    
+    def query_job(self, repo: str, query_string: str, event_id: str = None, number_of_events_after: int = None, number_of_events_before: int = None, timestamp: int = None, allow_event_skipping: bool = False, arguments: Dict = None, around: str = None, auto_bucket_count: int = None, end: str = 'now', include_delete_events: bool = None, ingest_end: str = None, ingest_start: str = None, is_alert_query: bool = None, is_interactive: bool = False, is_live: bool = False, is_repeating_subquery: bool = False, language_version: str = None, no_result_until_done: bool = False, show_query_event_distribution: bool = False, start: str = '24hours', time_zone: str = None, time_zone_offset_minutes: int = None, use_ingest_time: bool = False):
+        data = {'allowEventSkipping': allow_event_skipping,
+                'arguments': arguments,
+                'around': around,
+                'eventId': event_id,
+                'numberOfEventsAfter': number_of_events_after,
+                'numberOfEventsBefore': number_of_events_before,
+                'timestamp': timestamp,
+                'autoBucketCount': auto_bucket_count,
+                'end': end,
+                'includeDeletedEvents': include_delete_events,
+                'ingestEnd': ingest_end,
+                'ingestStart': ingest_start,
+                'isAlertQuery': is_alert_query,
+                'isInterative': is_interactive,
+                'isLive': False,
+                'isRepeatingSubquery': is_repeating_subquery,
+                'languageVersion': language_version,
+                'noResultUntilDone': no_result_until_done,
+                'queryString': query_string,
+                'showQueryEventDistribution': show_query_event_distribution,
+                'start': start,
+                'time_zone': time_zone,
+                'timeZoneOffsetMinutes': time_zone_offset_minutes}
+        result = self._rest_adapter.post(endpoint=f'/repositories/{repo}/queryjobs', data=data)
+        return QueryJob(result.data)
