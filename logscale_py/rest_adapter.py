89daaa5dc13d1b7295d37275018f1d8f330f94dd
milesjniemeyer <miles@mjniemeyer.com>
2024-06-25 14:01:13 -0400

    Added stream parameter to rest adapter

--- logscale_py/rest_adapter.py
+++ logscale_py/rest_adapter.py
@@ -5,7 +5,7 @@ import requests.packages
 from json import JSONDecodeError
 from typing import List, Dict
 from logscale_py.exceptions import LogScalePyException
-from logscale_py.models import Result
+from logscale_py.models import *
 
 class RestAdapter:
     def __init__(self, hostname: str, api_token: str, version: str = 'v1', ssl_verify: bool = True, logger: logging.Logger = None):
@@ -17,23 +17,23 @@ class RestAdapter:
         :param ssl_verify: (optional) Normally set to True, but if having SSL/TLS cert validation issues, can turn off with False
         :param logger: (optional) If your app has a logger, pass it in here.
         """
-        self.url = 'https://{}/api/{}/'.format(hostname, version)
+        self.url = 'https://{}/api/{}'.format(hostname, version)
         self._api_token = api_token
         self._ssl_verify = ssl_verify
         if not ssl_verify:
             requests.packages.urllib3.disable_warnings()
         self._logger = logger or logging.getLogger(__name__)
 
-    def _do(self, http_method: str, endpoint: str, ep_params: Dict = None, data: Dict = None) -> Result:
+    def _do(self, http_method: str, endpoint: str, ep_params: Dict = None, data: Dict = None, stream: bool = False) -> Result:
         full_url = self.url + endpoint
-        headers = {'Authorization': f'Bearer {self._api_token}', 'Accept': 'application/json'}
+        headers = {'Authorization': f'Bearer {self._api_token}', 'Accept': 'application/json', 'Content-Type': 'application/json'}
         log_line_pre = f"method={http_method}, url={full_url}, params={ep_params}"
         log_line_post = ', '.join((log_line_pre, "success={}, status_code={}, message={}"))
         
         # Log HTTP params and perform an HTTP request, catching and re-raising any exceptions
         try:
             self._logger.debug(msg=log_line_pre)
-            response = requests.request(method=http_method, url=full_url, verify=self._ssl_verify, headers=headers, params=ep_params, json=data)
+            response = requests.request(method=http_method, url=full_url, verify=self._ssl_verify, headers=headers, params=ep_params, json=data, stream=stream)
         except requests.exceptions.RequestException as e:
             self._logger.error(msg=(str(e)))
             raise LogScalePyException('Request failed') from e
@@ -54,28 +54,28 @@ class RestAdapter:
         self._logger.error(msg=(log_line))
         raise LogScalePyException(f'{response.status_code}: {response.reason}')
 
-    def get(self, endpoint: str, ep_params: Dict = None) -> Result:
+    def get(self, endpoint: str, ep_params: Dict = None, stream: bool = False) -> Result:
         """
         GET HTTP method
         :param endpoint: The endpoint on the LogScale API beging accessed
         :ep_params: Parameters to be passed in the URL query string
         """
-        return self._do(http_method='GET', endpoint=endpoint, ep_params=ep_params)
+        return self._do(http_method='GET', endpoint=endpoint, ep_params=ep_params, stream=stream)
 
-    def post(self, endpoint: str, ep_params: Dict = None, data: Dict = None) -> Result:
+    def post(self, endpoint: str, ep_params: Dict = None, data: Dict = None, stream: bool = False) -> Result:
         """
         POST HTTP method
         :param endpoint: The endpoint on the LogScale API beging accessed
         :param ep_params: Parameters to be passed in the URL query string
         :param data: Parameters to be passed in the body of the request
         """
-        return self._do(http_method='POST', endpoint=endpoint, ep_params=ep_params, data=data)
+        return self._do(http_method='POST', endpoint=endpoint, ep_params=ep_params, data=data, stream=stream)
     
-    def delete(self, endpoint: str, ep_params: Dict = None, data: Dict = None) -> Result:
+    def delete(self, endpoint: str, ep_params: Dict = None, data: Dict = None, stream: bool = False) -> Result:
         """
         DELETE HTTP method
         :param endpoint: The endpoint on the LogScale API beging accessed
         :param ep_params: Parameters to be passed in the URL query string
         :param data: Parameters to be passed in the body of the request
         """
-        return self._do(http_method='DELETE', endpoint=endpoint, ep_params=ep_params, data=data)
+        return self._do(http_method='DELETE', endpoint=endpoint, ep_params=ep_params, data=data, stream=stream)
