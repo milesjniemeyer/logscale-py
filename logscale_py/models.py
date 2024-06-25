6847d35fb5d90fd12bc17bbc7509b2ebc50d6d65
milesjniemeyer <miles@mjniemeyer.com>
2024-06-25 15:54:16 -0400

    Updated query family response models

--- logscale_py/models.py
+++ logscale_py/models.py
@@ -13,24 +13,15 @@ class Result:
         self.data = data if data else []
 
 class Query:
-    def __init__(self, **kwargs: Any):
-        self.data: Dict = kwargs
-    
-    @classmethod
-    def from_dict(cls, data_dict: Dict) -> 'Query':
-        return cls(**data_dict)
-    
-class QueryList(list):
-    @property
-    def data(self):
-        return [query.data for query in self]
-    
-class QueryJob():
     def __init__(self, data):
-        """
-        Constructor for QueryJobResponse
-        :param data: Python Dictionary containing the response data
-        """
+        self.data = data
+    
+    def __str__(self):
+        return str(self.data)
+    
+class QueryJob:
+    def __init__(self, data):
+        self.data = data
         self.hashed_query_on_view = data.get('hashedQueryOnView')
         self.id = data.get('id')
         self.query_on_view = data.get('queryOnView')
@@ -38,4 +29,18 @@ class QueryJob():
         self.execution_mode = self.static_meta_data.get('executionMode') if self.static_meta_data else None
 
     def __str__(self):
-        return f'QueryJob(hashed_query_on_view={self.hashed_query_on_view}, id={self.id}, query_on_view={self.query_on_view}, static_meta_data={self.static_meta_data}, execution_mode={self.execution_mode})'
+        return str(self.data)
+    
+class PollQuery:
+    def __init__(self, data):
+        self.data = data
+        self.cancelled = data.get('cancelled')
+        self.done = data.get('done')
+        self.events = Query(data.get('events')) if data.get('events') else None
+        self.filter_matches = data.get('filterMatches')
+        self.meta_data = data.get('metaData')
+        self.query_event_distribution = data.get('queryEventDistribution')
+        self.warnings = data.get('warnings')
+
+    def __str__(self):
+        return str(self.data)
