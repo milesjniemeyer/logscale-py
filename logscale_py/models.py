57b4fb51010f7c606b9e4bf383afabb86c89bfc5
milesjniemeyer <miles@mjniemeyer.com>
2024-06-25 14:39:52 -0400

    query_job endpoint response model

--- logscale_py/models.py
+++ logscale_py/models.py
@@ -24,3 +24,18 @@ class QueryList(list):
     @property
     def data(self):
         return [query.data for query in self]
+    
+class QueryJob():
+    def __init__(self, data):
+        """
+        Constructor for QueryJobResponse
+        :param data: Python Dictionary containing the response data
+        """
+        self.hashed_query_on_view = data.get('hashedQueryOnView')
+        self.id = data.get('id')
+        self.query_on_view = data.get('queryOnView')
+        self.static_meta_data = data.get('staticMetaData')
+        self.execution_mode = self.static_meta_data.get('executionMode') if self.static_meta_data else None
+
+    def __str__(self):
+        return f'QueryJob(hashed_query_on_view={self.hashed_query_on_view}, id={self.id}, query_on_view={self.query_on_view}, static_meta_data={self.static_meta_data}, execution_mode={self.execution_mode})'
